3180652d1c948ce160f0d7e9a0e4e404bd9744bf
HENRIQUE GUIMARAES <118493830+bkhenrique@users.noreply.github.com>
2024-09-30 23:38:02 -0300

    Hello Word

---
+++ PrimeiroFundamentos/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// metodo novo Console.WriteLine("Hello, World!");
+
+//usando metodo antigo
+
+using System;
+
+namespace PrimeiroFundamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word");
+        }
+    }
+}
