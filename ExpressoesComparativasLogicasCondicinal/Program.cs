68f0aca157f25e3e85e55ddedc408605dc60574d
HENRIQUE GUIMARAES <118493830+bkhenrique@users.noreply.github.com>
2024-10-02 21:04:31 -0300

    mais testes

---
+++ ExpressoesComparativasLogicasCondicinal/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressoesComparativasLogicasCondicinal
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Operadores Conparativos
+            // > maior
+            // < menor
+            // >= maior ou igual
+            // <= menor ou igual
+            // == igual
+            // != diferente
+
+
+            //  int X = 5;
+            //  int Y = 10;
+
+            //  X == 5 true
+            //  Y == 10 true
+
+            //  X > Y false
+            //  X < Y true
+            //  X == Y false
+            //  X != Y true
+
+
+            // Operadores Logicos
+
+            // && = E
+            // || = ou
+            // ! = Não ou negativa
+
+            // Extrutura Condicional
+
+            //  if = se
+            // else = se não
+
+
+
+            // Switch-case
+
+            //exemplo
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch (x)
+            {
+                case 1:
+                    dia = "domingo";
+                    break;
+                case 2:
+                    dia = "segunda";
+                    break;
+                case 3:
+                    dia = "terça";
+                    break;
+                case 4:
+                    dia = "quarta";
+                    break;
+                case 5:
+                    dia = "quinta";
+                    break;
+                case 6:
+                    dia = "sexta";
+                    break;
+                case 7:
+                    dia = "sabado";
+                    break;
+                default:
+                    dia = "Digite de 1 a 7";
+                    break;
+
+
+            }
+            Console.WriteLine("Dia da semana =" + dia);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ExtruturaRepeticao/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // while = enquanto
+        // É uma estrutura de controle que
+        // repete um bloco de comandos
+        // enquanto uma condição for
+        // verdadeira.
+        // Quando usar: quando não se
+        // sabe previamente a quantidade
+        // de repetições que será realizada.
+
+
+
+        //  while (condição)
+        //  {
+        //      comando 1
+        //      comando 2
+        //  }
+
+        int x, soma;
+
+        x = int.Parse(Console.ReadLine());
+        soma = 0;
+
+        while (x != 0 )
+        {
+            soma = soma + x;
+            x = int.Parse(Console.ReadLine());
+
+        }
+        Console.WriteLine(soma);
+        Console.ReadLine();
+
+
+
+
+
+        // for = para
+
+        //  É uma estrutura de controle que
+        // repete um bloco de comandos
+        // para um certo intervalo de
+        // valores.
+        // Quando usar: quando se sabe
+        // previamente a quantidade de
+        // repetições, ou o intervalo de
+        // valores.
+
+
+        // for (início; condição; incremento)
+        // {
+        //     comando 1
+        //     comando 2
+        // }
+
+        for (int i = 0; i < 5; i++) 
+        {
+            Console.WriteLine("Valor de i: " + i);
+            Console.ReadLine();
+        }
+    }
+}
